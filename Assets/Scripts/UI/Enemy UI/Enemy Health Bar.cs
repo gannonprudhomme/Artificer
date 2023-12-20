5de22b7f20feeea65b958a6e7e0621bc03a3373d
Gannon Prudhomme <gannonprudhomme@gmail.com>
2023-12-19 18:46:36 -0600

    Hooked up freeze outline UI

--- Assets/Scripts/UI/Enemy UI/Enemy Health Bar.cs
+++ Assets/Scripts/UI/Enemy UI/Enemy Health Bar.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyHealthBar : MonoBehaviour {
+    [Tooltip("Image to be used for the health fill %")]
     public Image FillImage;
 
-    [Tooltip("Health of the enemy this is displaying for")]
-    public Health health;
+    [Tooltip("Reference to the parent game object which contains the frozen outline image that displays over the health bar when the enemy is frozen")]
+    public GameObject FrozenOutline;
+    
+    [Tooltip("The entity (enemy) this is displaying for")]
+    public Entity entity;
 
     // Transform of the target this should look at
     // Set by the Enemy that owns this
     public Transform Target { get; set; }
 
     void Update() {
-        if (health == null) {
+        if (entity.health == null) {
             Debug.LogError($"{this.name}'s health was not passed, returning");
             return;
         }
 
         // Set fill based on % of health the enemy has
-        FillImage.fillAmount = (int) health.CurrentHealth / health.MaxHealth;
+        FillImage.fillAmount = (int) entity.health.CurrentHealth / entity.health.MaxHealth;
+
+        // Hide / show the health bar frozen "outline" if the enemy is frozen
+        FrozenOutline.SetActive(entity.GetIsFrozen());
     }
 }
