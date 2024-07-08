7185e0f32500a3afd52022a33adf46fa55f8a424
Gannon Prudhomme <gannonprudhomme@gmail.com>
2024-07-08 09:49:30 -0700

    made enemy health bar hide at 100%

--- Assets/Scripts/UI/Enemy UI/Enemy Health Bar.cs
+++ Assets/Scripts/UI/Enemy UI/Enemy Health Bar.cs
@@ -3,31 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+#nullable enable
+
 public class EnemyHealthBar : MonoBehaviour {
     [Tooltip("Image to be used for the health fill %")]
-    public Image FillImage;
+    public Image? FillImage;
 
     [Tooltip("Reference to the parent game object which contains the frozen outline image that displays over the health bar when the enemy is frozen")]
-    public GameObject FrozenOutline;
+    public GameObject? FrozenOutline;
+
+    [Tooltip("Canvas that contains the healthbar. Used to hide / show it when the enemy is at full health")]
+    public Canvas? CanvasContainer; 
     
     [Tooltip("The entity (enemy) this is displaying for")]
-    public Entity entity;
+    public Entity? entity;
 
     private void Start() {
-        entity.health.OnDeath += OnDeath;
+        entity!.health!.OnDeath += OnDeath;
     }
 
     void Update() {
-        if (entity.health == null) {
+        if (entity!.health == null) {
             Debug.LogError($"{this.name}'s health was not passed, returning");
             return;
         }
 
+        float percent = entity.health.CurrentHealth / entity.health.MaxHealth;
+        if (percent >= 0.9999f) {
+            CanvasContainer!.enabled = false;
+        } else {
+            CanvasContainer!.enabled = true;
+        }
+
         // Set fill based on % of health the enemy has
-        FillImage.fillAmount = (int) entity.health.CurrentHealth / entity.health.MaxHealth;
+        FillImage!.fillAmount = ((int) entity.health.CurrentHealth) / entity.health.MaxHealth;
 
         // Hide / show the health bar frozen "outline" if the enemy is frozen
-        FrozenOutline.SetActive(entity.GetIsFrozen());
+        FrozenOutline!.SetActive(entity.GetIsFrozen());
     }
 
     private void OnDeath() {
